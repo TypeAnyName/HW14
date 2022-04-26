9977f80dac793f145488092652b8d4633b26258c
TypeAnyName <alexgox11@gmail.com>
2022-04-26 22:03:45 +0300

    Реализовал второй шаг из домашки и сделал вьюшку

--- main.py
+++ main.py
@@ -22,8 +22,26 @@ def main():
             "country": response[1],
             "release_year": response[2],
             "genre": response[3],
-            "description": response[4],
+            "description": response[4].strip(),
         }
+        return jsonify(response_json)
+
+    @app.route("/movie/<int:start>/to/<int:end>")
+    def release_year_search(start, end):
+        query = f"""
+                    SELECT title, release_year
+                    FROM netflix
+                    WHERE release_year BETWEEN {start} AND {end}
+                    ORDER BY release_year
+                    LIMIT 100
+            """
+        response = utils.bd_connect(query)
+        response_json = []
+        for film in response:
+            response_json.append({
+                "title": film[0],
+                "release_year": film[1],
+            })
         return jsonify(response_json)
 
     app.run(debug=True)
